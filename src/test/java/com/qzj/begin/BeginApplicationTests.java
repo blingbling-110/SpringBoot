5356c9dcb20de864d4fe298c26463843296622c4
blingbling-110 <523497359@qq.com>
2019-08-22 16:08:09 +0800

    自定义日志配置 26 P28 P33

--- src/test/java/com/qzj/begin/BeginApplicationTests.java
+++ src/test/java/com/qzj/begin/BeginApplicationTests.java
@@ -3,6 +3,8 @@ package com.qzj.begin;
 import com.qzj.begin.bean.YmlTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -26,6 +28,9 @@ public class BeginApplicationTests {
 	@Autowired
 	ApplicationContext ioc;
 
+	//记录器
+	Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Test
 	public void testHelloService(){
 		//判断有没有指定的bean
@@ -38,6 +43,15 @@ public class BeginApplicationTests {
 	@Test
 	public void contextLoads() {
 		System.out.println(ymlTest);
+		//日志的级别；
+		// 由低到高 trace<debug<info<warn<error
+		// 可以调整输出的日志级别；日志就只会在这个级别以以后的高级别生效
+		logger.trace("这是trace日志...");
+		logger.debug("这是debug日志...");
+		//SpringBoot默认给我们使用的是info级别的，没有指定级别的就用SpringBoot默认规定的级别；root级别
+		logger.info("这是info日志...");
+		logger.warn("这是warn日志...");
+		logger.error("这是error日志...");
 	}
 
 }
