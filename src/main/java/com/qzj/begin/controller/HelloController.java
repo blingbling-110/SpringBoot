3dd36a03642975aa0455142d9bb62c76c1302883
blingbling-110 <523497359@qq.com>
2019-08-29 22:00:36 +0800

    Thymeleaf 32 P37 P39

--- src/main/java/com/qzj/begin/controller/HelloController.java
+++ src/main/java/com/qzj/begin/controller/HelloController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.Map;
+
 /**
  * @Controller 表示它能处理浏览器请求
  */
@@ -25,5 +28,12 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
         return "Hello, Spring Boot!" + who;
+    }
+
+    @RequestMapping("/success")
+    public String success(Map<String, Object> map) {
+        map.put("hello", "<h1>你好<h1>");
+        map.put("users", Arrays.asList("超级管理员", "管理员", "用户"));
+        return "success";
     }
 }
