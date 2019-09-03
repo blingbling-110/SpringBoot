86f537cee8c74c0cdfc2c7786c2fccc7379b1588
blingbling-110 <523497359@qq.com>
2019-09-03 14:00:48 +0800

    引入crud资源 35 P41 P39

--- src/main/java/com/qzj/begin/config/MvcConfig.java
+++ src/main/java/com/qzj/begin/config/MvcConfig.java
@@ -1,15 +1,20 @@
 package com.qzj.begin.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //实现WebMvcConfigurer接口可以扩展SpringMVC的功能
+//@EnableWebMvc 全面接管SpringMVC
+//@EnableWebMvc
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         //"/qzj"请求映射到"success"
         registry.addViewController("/qzj").setViewName("success");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
     }
 }
