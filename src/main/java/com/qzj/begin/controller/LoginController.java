34e10400430ed19fca8fd9340d50339d6f591d11
blingbling-110 <523497359@qq.com>
2019-09-06 16:30:51 +0800

    登录 36 P45 P39

---
+++ src/main/java/com/qzj/begin/controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.qzj.begin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class LoginController {
+    @PostMapping("/user/login")
+    //以上等价于@RequestMapping(value = "/user/login", method = RequestMethod.POST)
+    //@RequestParam 将请求的参数与处理方法的参数绑定
+    public String login(@RequestParam("username") String usr,
+                        @RequestParam("password") String pwd,
+                        Map<String, Object> map) {
+        if (!StringUtils.isEmpty(usr) && "123456".equals(pwd))
+            return "dashboard";
+        else {
+            map.put("msg", "用户名密码错误");
+            return "index";
+        }
+    }
+}
