09ff6d3a573d3651347d5f61d3e484bfa3bf1143
blingbling-110 <523497359@qq.com>
2019-09-23 16:44:24 +0800

    Thymeleaf模板布局 38 P47 P40

---
+++ src/main/java/com/qzj/begin/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.qzj.begin.controller;
+
+import com.qzj.begin.dao.EmployeeDao;
+import com.qzj.begin.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @GetMapping("/emps")
+    public String list(Model model){
+        Collection<Employee> employees = employeeDao.getAll();
+        //放在请求域中
+        model.addAttribute("emps", employees);
+        return "list";
+    }
+}
