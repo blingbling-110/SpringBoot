dff97664735df7eab741f5d96fd7b38ea47f2ade
blingbling-110 <523497359@qq.com>
2019-10-02 09:56:24 +0800

    员工添加 41 P49 P40

--- src/main/java/com/qzj/begin/controller/EmployeeController.java
+++ src/main/java/com/qzj/begin/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.qzj.begin.controller;
 
+import com.qzj.begin.dao.DepartmentDao;
 import com.qzj.begin.dao.EmployeeDao;
+import com.qzj.begin.entities.Department;
 import com.qzj.begin.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,11 +16,21 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
+    @Autowired
+    DepartmentDao departmentDao;
+
     @GetMapping("/emps")
     public String list(Model model){
         Collection<Employee> employees = employeeDao.getAll();
         //放在请求域中
         model.addAttribute("emps", employees);
         return "list";
+    }
+
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "add";
     }
 }
